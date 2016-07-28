36d1a510e456ae0ef57775c02c789ba0e141a8bb
Oleg Bogatyryev <olegbogatyryev@MacBook-Pro-Oleg.local>
2016-07-28 17:45:07 +0300

    Validation init commit. Tests init commit

--- js-exercises/dice_expression.js
+++ js-exercises/dice_expression.js
@@ -2,9 +2,40 @@
 
 var random = require("random-js")(); // uses the nativeMath engine
 
-function DiceExpression(paramter) {
-    var value = random.integer(0, 99);
-    this.diceExpression = paramter;
+function DiceMember(expression){
+    this.expression = expression;
+    this.regex =/^(\+|-)?([1-9][0-9]*)?(?:(?:d|D)?([1-9][0-9]*)+|d%|D%)$/;
+    
+}
+
+DiceMember.prototype.validate = function(){
+    return this.regex.test(this.expression);
+};
+// DiceMember.prototype.validate = function(){
+//     return this.expression.match(this.expressionValidRegex).length();
+// };
+
+function DiceExpression(input) {
+    
+    this.inputMembers = input;
+    
+    /**
+     * Regular expression for complex dise expression
+     * @type {RegExp}
+     */
+    this.regex =/^((?:\+|-)?(?:[1-9][0-9]*)?(?:(?:d|D)?(?:[1-9][0-9]*)+|d%|D%))+$/;
+    // ((?:\+|-)?(?:[1-9][0-9]*)?(?:(?:d|D)?(?:[1-9][0-9]*)+|d%|D%))+
+    this.valid = this.regex.test(input);
+    
+    var variables = this.inputMembers.match(this.regex);
+    console.log(variables);
+    if(variables) {
+        console.log(" >> " + variables.length);
+        variables.forEach(function () {
+            debugger;
+        });
+    }
+    // this.diceExpression = paramter;
 }
 
 DiceExpression.prototype.min = function(){
@@ -22,5 +53,10 @@ DiceExpression.prototype.random = function (){
 DiceExpression.prototype.expression = function (){
     return this.diceExpression;
 };
+
+DiceExpression.prototype.validate = function(){
+    return this.valid;
+};
+
 
 module.exports = DiceExpression;
--- js-exercises/test/index.js
+++ js-exercises/test/index.js
@@ -4,10 +4,37 @@ var assert = require('assert')
 
 exports['test that logs all failures'] = function(assert) {
     var dicea = new DiceExpression("2d5");
-    console.log(dicea.min())
+    console.log(dicea.min());
     assert.equal(dicea.min(), -100, 'assert failure is logged');
     assert.equal(3 + 2, 5, 'assert pass is logged');
     assert.equal(dicea.expression(), 5, 'assert pass is logged');
-}
+};
+
+
+exports['test input parameters validation'] = function(assert) {
+    checkCorrectArray(["2d5", "D%", "d34567890", "87", "-54635", "+67", "3d%", "8d6+4-8-d%"]);
+
+    checkIncorrectArray(["d", "D", "gsdgsdf", "01", "0000000", "_", "0d0d","6d4+", ""]);
+
+    function checkCorrectArray(array){
+        array.forEach(checkCorrect);
+    }
+
+    function checkIncorrectArray(array){
+        array.forEach(checkIncorrect);
+    }
+    
+    function checkCorrect(expression){
+        assertExpression(expression, true);
+    }
+
+    function checkIncorrect(expression){
+        assertExpression(expression, false);
+    }
+
+    function assertExpression(expression, result){
+        assert.equal(new DiceExpression(expression).validate(), result, 'Dice Expression test ' + expression +"\'");
+    }
+};
 
 if (module == require.main) require('test').run(exports)
