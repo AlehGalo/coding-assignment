92f4c3566effa628d63195806bb698fe8109ccb0
Oleg Bogatyryev <olegbogatyryev@MacBook-Pro-Oleg.local>
2016-07-29 16:10:40 +0300

    Tests number increased. Logical functional added.

--- js-exercises/dice_expression.js
+++ js-exercises/dice_expression.js
@@ -4,59 +4,166 @@ var random = require("random-js")(); // uses the nativeMath engine
 
 function DiceMember(expression){
     this.expression = expression;
-    this.regex =/^(\+|-)?([1-9][0-9]*)?(?:(?:d|D)?([1-9][0-9]*)+|d%|D%)$/;
+
+    this.isConstant = false;    
+    this.negative = expression.startsWith("-");
     
+    this.startNumber = 1;
+    this.endNumber = 1;
+
+
+    var splitChar = getSplitChar(this.expression);
+
+    if (splitChar){
+        var numbers = getNumbers();
+        this.startNumber = numbers[0];
+        this.endNumber = numbers[1];
+    }else {
+        this.isConstant = true;
+        this.startNumber = 1;
+        this.endNumber = expression;
+    }
+    
+    function getNumbers(){
+        var numbers = splitNumbers(splitChar);
+        var number1 = parseInt(numbers[0]);
+        if(isNaN(number1)){
+            number1 = 1;
+        }
+        var number2 = parseInt(numbers[1]);
+        if(isNaN(number2)){
+            number2 = 100;
+        }
+        return [number1, number2];
+    }
+    
+    function splitNumbers(splitLetter){
+        if(expression.includes(splitLetter)){
+            return expression.split(splitLetter);
+        }
+        return [];
+    }
+    
+    function getSplitChar(expr){
+        var splitChar = /(d|D)/g.exec(expr);
+        if(splitChar){
+            return splitChar[1];
+        }
+        return null;
+    }
 }
 
-DiceMember.prototype.validate = function(){
-    return this.regex.test(this.expression);
+DiceMember.prototype.min = function(){
+    if(this.isConstant){
+        return this.endNumber;
+    }
+    return this.startNumber;
 };
-// DiceMember.prototype.validate = function(){
-//     return this.expression.match(this.expressionValidRegex).length();
-// };
+
+DiceMember.prototype.isnegative = function(){
+    return this.negative;
+};
+
+DiceMember.prototype.max = function(){
+    return this.startNumber*this.endNumber;
+};
+
+DiceMember.prototype.rollDetailed = function(){
+    if(this.isConstant){
+        return {roll: this.endNumber, dice: [this.endNumber]};
+    } else {
+        var rollSum = 0, all = [];
+        for (var i = 0; i < this.startNumber; i++) {
+            var value = random.integer(1, this.endNumber);
+            rollSum += value;
+            all.push(value);
+        }
+        return {roll: rollSum, dice: all};
+    }
+};
+
+
+DiceMember.prototype.roll = function(){
+    if(this.isConstant){
+        return this.endNumber;
+    }
+        var rollSum = 0;
+        for (var i = 0; i < this.startNumber; i++) {
+            rollSum += random.integer(1, this.endNumber)
+        }
+        return rollSum;
+
+};
 
 function DiceExpression(input) {
     
     this.inputMembers = input;
-    
+    this.dices = [];
     /**
      * Regular expression for complex dise expression
      * @type {RegExp}
      */
-    this.regex =/^((?:\+|-)?(?:[1-9][0-9]*)?(?:(?:d|D)?(?:[1-9][0-9]*)+|d%|D%))+$/;
-    // ((?:\+|-)?(?:[1-9][0-9]*)?(?:(?:d|D)?(?:[1-9][0-9]*)+|d%|D%))+
+    this.regex =/^((?:\+|-)?(?:[1-9][0-9]*)?(?:(?:d|D)?(?:[1-9][0-9]*)+|d%|D%))+$/g;
+    this.regexInner = /((?:\+|-)?(?:[1-9][0-9]*)?(?:(?:d|D)?(?:[1-9][0-9]*)+|d%|D%))/g;
+
     this.valid = this.regex.test(input);
-    
-    var variables = this.inputMembers.match(this.regex);
-    console.log(variables);
-    if(variables) {
-        console.log(" >> " + variables.length);
-        variables.forEach(function () {
-            debugger;
-        });
+
+    if(this.valid){
+        var matches;
+        while (matches = this.regexInner.exec(input)) {
+            this.dices.push(new DiceMember(matches[1]));
+        }
     }
+
     // this.diceExpression = paramter;
 }
 
 DiceExpression.prototype.min = function(){
-    return -100;
+    var sum = 0;
+
+    this.dices.forEach(function callRoll(diceMember){
+        var value = parseInt(diceMember.min());
+        sum += value;//diceMember.isnegative() ? -value: value;
+    });
+    return sum;
 };
 
 DiceExpression.prototype.max = function () {
-    return 100;
-};
+    var sum = 0;
 
-DiceExpression.prototype.random = function (){
-  return random.integer(0, 100);
+    this.dices.forEach(function callRoll(diceMember){
+        sum += parseInt(diceMember.max());//diceMember.isnegative() ? -diceMember.max(): diceMember.max();
+    });
+    return sum;
 };
 
 DiceExpression.prototype.expression = function (){
-    return this.diceExpression;
+    return this.inputMembers;
 };
 
 DiceExpression.prototype.validate = function(){
     return this.valid;
 };
 
+DiceExpression.prototype.roll = function(){
+    var sum = 0;
+    
+    this.dices.forEach(function(diceMember){
+        sum += parseInt(diceMember.roll());
+    });
+    return sum;
+};
+
+DiceExpression.prototype.rollDetailed = function(){
+    var res = [];
+    var sum = 0;
+
+    this.dices.forEach(function(diceMember){
+        var rollDetailed = diceMember.rollDetailed();
+        res.push(rollDetailed["dice"]);
+        sum += parseInt(rollDetailed["roll"]);
+    });
+    return {roll: sum, dice: res};
+};
 
 module.exports = DiceExpression;
--- js-exercises/test/index.js
+++ js-exercises/test/index.js
@@ -1,19 +1,30 @@
 'use strict';
 var DiceExpression = require('../dice_expression.js');
-var assert = require('assert')
+var assert = require('assert');
 
-exports['test that logs all failures'] = function(assert) {
-    var dicea = new DiceExpression("2d5");
-    console.log(dicea.min());
-    assert.equal(dicea.min(), -100, 'assert failure is logged');
-    assert.equal(3 + 2, 5, 'assert pass is logged');
-    assert.equal(dicea.expression(), 5, 'assert pass is logged');
+var expressions = ["2d5", "d18", "d%", "5d%", "3D678","-8", "-5D2", "4D9-9+3"];
+var results = [[2, 10], [1, 18], [1, 100], [5, 500], [3, 2034], [-8, -8], [-5, -10], [-2, 30]];
+
+exports['test dice expression'] = function(assert) {
+    makeTest(expressions, results);
+    function makeTest(expressionArray, resultsArray){
+        var counter = 0;
+        expressionArray.forEach(function(item){
+            var diceExpre = new DiceExpression(item);
+            
+            var resArray = resultsArray[counter];
+            var min = resArray[0], max = resArray[1]; 
+            
+            assert.equal(diceExpre.min(), min, 'min value ' + min);
+            assert.equal(diceExpre.max(), max, 'max value ' + max);
+            ++counter;
+        });
+    }
 };
 
 
 exports['test input parameters validation'] = function(assert) {
     checkCorrectArray(["2d5", "D%", "d34567890", "87", "-54635", "+67", "3d%", "8d6+4-8-d%"]);
-
     checkIncorrectArray(["d", "D", "gsdgsdf", "01", "0000000", "_", "0d0d","6d4+", ""]);
 
     function checkCorrectArray(array){
@@ -33,8 +44,29 @@ exports['test input parameters validation'] = function(assert) {
     }
 
     function assertExpression(expression, result){
-        assert.equal(new DiceExpression(expression).validate(), result, 'Dice Expression test ' + expression +"\'");
+        assert.equal(new DiceExpression(expression).validate(), result,
+            'Dice Expression validation test ' + expression +"\'");
     }
 };
 
-if (module == require.main) require('test').run(exports)
+exports['test dice roll validation'] = function(assert) {
+    makeTest(expressions, results, 10);
+    function makeTest(expressionArray, minMaxArray, repeatTime){
+        var counter = 0;
+            expressionArray.forEach(function (item) {
+                var diceExp = new DiceExpression(item);
+                var itemToCheck = minMaxArray[counter];
+                for(var i=0; i< repeatTime; i++) {
+                    var rollValue = diceExp.roll();
+                    var operationsResult = rollValue >= itemToCheck[0] 
+                        && rollValue <= itemToCheck[1];
+                    assert.ok(operationsResult,
+                        "Dice expression checked " + rollValue + " -> [" + itemToCheck[0] 
+                        + "..." + itemToCheck[1] + "]");
+                }
+                ++counter;
+            });
+    }
+};
+
+if (module == require.main) require('test').run(exports);
